cbd327aaf1db9f5d79453911d254518744f1bb1e
Jose María Gestal <jmgestal00@gmail.com>
2024-06-06 21:29:41 +0200

    06-06

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,16 +2,10 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        http_index: './js/http/http_index.js',
-        http_juego: './js/http/http_juego.js',
-        http_login: './js/http/http_login.js',
-        http_registro: './js/http/http_registro.js',
-        constants: './js/constants.js',
         index: './js/index.js',
         juego: './js/juego.js',
         login: './js/login.js',
         registro: './js/registro.js',
-        user: './js/user.js',
         
     },
 
